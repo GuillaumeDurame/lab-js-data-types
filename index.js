b09ad92ecffc5656c925fcb11259e9a1f2c714e4
Guillaume Duramé <durameg@gmail.com>
2024-04-10 15:47:24 +0200

    Solved lab

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister =`${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`
 
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -22,10 +22,15 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
-
+const lastPart1 = part1.charAt(part1.length -1);
+const lastPart2 = part2.charAt(part2.length -1);
+const majPart1 = lastPart1.toUpperCase();
+const majPart2 = lastPart2.toUpperCase();
+const newPart1 = part1.slice(0, -1) + majPart1;
+const newPart2 = part2.slice(0, -1) + majPart2;
+const result = `${newPart1}${newPart2}`;
 // Print the cameLtaiL-formatted string
-
+console.log(result);
 
 
 
@@ -35,10 +40,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal*15/100;
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -47,10 +52,13 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
-
+function generateRandomNumber(min,max)
+{
+    return Math.floor(Math.random()*(max - min + 1)) + min;
+}
+const randomNumber = generateRandomNumber(1,10);
 // Print the generated random number
-
+console.log(`${randomNumber}`)
 
 
 /*******************************************
@@ -62,15 +70,24 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+/*False*/
 const expression2 = a || b;
-
+/*True*/
 const expression3 = !a && b;
-
+/*False*/
 const expression4 = !(a && b);
-
+/*True*/
 const expression5 = !a || !b;
-
+/*True*/
 const expression6 = !(a || b);
-
+/*False*/
 const expression7 = a && a;
+/*True*/
+
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
+console.log(expression5);
+console.log(expression6);
+console.log(expression7);
